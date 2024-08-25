b15eff1d83f64dc0677acedecbfc2928fd3cffca
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-08-25 16:11:44 +0530

    create table notes

--- src/main/java/com/example/noteTaker/config/WebAppConfig.java
+++ src/main/java/com/example/noteTaker/config/WebAppConfig.java
@@ -2,10 +2,14 @@ package com.example.noteTaker.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan(basePackages = "com.example.noteTaker")
 @EnableWebMvc
+@EnableJpaRepositories
+@EnableTransactionManagement
 public class WebAppConfig {
 }
