b07c893184b9a39bdb9f2dfec3f11f9157edbd60
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-08-25 15:48:57 +0530

    get a DriverManagerDataSource

--- src/main/java/com/example/noteTaker/config/WebAppRootConfig.java
+++ src/main/java/com/example/noteTaker/config/WebAppRootConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -24,9 +25,12 @@ import javax.sql.DataSource;
 public class WebAppRootConfig {
     @Bean
     public DataSource dataSource() {
-
-        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-        return builder.setType(EmbeddedDatabaseType.HSQL).build();
+        var dmbs = new DriverManagerDataSource();
+        dmbs.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dmbs.setUrl("jdbc:mysql://localhost:3306/noteTaker?createDatabaseIfNotExist=true");
+        dmbs.setUsername("root");
+        dmbs.setPassword("Ijse@1234");
+        return dmbs;
     }
 
     @Bean
