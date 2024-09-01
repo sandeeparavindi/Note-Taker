5ffb0e5777a55ad85a8ff2dc5ee6a3ca326a3189
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-09-01 15:26:00 +0530

    genarate user id

--- src/main/java/com/example/noteTaker/util/AppUtil.java
+++ src/main/java/com/example/noteTaker/util/AppUtil.java
@@ -6,4 +6,9 @@ public class AppUtil {
     public static String createNote(){
         return "NOTE-"+ UUID.randomUUID().toString();
     }
+
+    public static String createUserId(){
+        return "USER"+ UUID.randomUUID().toString();
+    }
+
 }
